cd98f544bf5d92141eb509d56545c5ca6e97efbe
dhfsu <3227978070@qq.com>
2026-03-09 15:29:58 +0800

    使用RTK管理账单项目

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import sum from '@/test';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { Provider } from 'react-redux';
+import store from './store';
 import './theme.css'
 const total = sum(1,3)
 console.log(total);
@@ -14,6 +16,7 @@ root.render(
     
     // 严格模式有时候会造成钩子模式执行两次，会造成有疑惑的点
   // </React.StrictMode>
-
-  <RouterProvider router={router}></RouterProvider>
+  <Provider store={store}>
+    <RouterProvider router={router}></RouterProvider>
+  </Provider>
 );
---
+++ src/store/modules/billStore.js
@@ -0,0 +1,28 @@
+//编写账单列表相关的store
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const billStore = createSlice({
+    name: "bill",
+    initialState: {
+        billList: []
+    },
+    reducers: {
+        //同步修改方法
+        setBillList(state, action) {
+            state.billList = action.payload;
+        }
+    }
+})
+
+const { setBillList } = billStore.actions;
+const getBillList = () => {
+    return async (dispatch) => {
+        //编写异步请求
+        const res = await axios.get('http://localhost:8888/ka')
+        dispatch(setBillList(res.data))
+    }
+}
+const  reducer  = billStore.reducer;
+export default reducer;
+export { getBillList };
