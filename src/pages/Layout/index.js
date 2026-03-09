5a56055d996bea02f370feb7ce778adbe7fa096e
dhfsu <3227978070@qq.com>
2026-03-09 11:35:59 +0800

    完成主题颜色定制化

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,3 +1,4 @@
+import { Button } from "antd-mobile";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
@@ -6,6 +7,12 @@ const Layout = () => {
             这是一级Layout路由
             {/* 二级路由出口，不设置的话二级路由无法显示 */}
             <Outlet />
+            {/* 测试全局生效按钮 */}
+            <Button color="primary">测试全局</Button>
+            <div className="purple">
+                {/* 测试局部生效按钮 */}
+                <Button color="primary">测试全局</Button>
+            </div>
         </div>
     )
 }
