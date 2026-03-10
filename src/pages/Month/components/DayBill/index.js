c18830e9418d7dbbbcf5e274baa3a1251b768f05
dhfsu <3227978070@qq.com>
2026-03-10 11:02:41 +0800

    单日账单列表渲染

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
 import { useMemo } from 'react'
-
+import { billTypeToName } from '@/contants'
 
 const DailyBill = ({ date, billList }) => {
 
@@ -33,6 +33,22 @@ const DailyBill = ({ date, billList }) => {
                         <span className="balance">{dayResult.total.toFixed(2)}</span>
                     </div>
                 </div>
+            </div>
+            {/* 单日列表 */}
+            <div className="billList" >
+                {billList.map(item => {
+                    return (
+                        <div className="bill" key={item.id}>
+                            {/* 图标 */}
+                            <div className="detail">
+                                <div className="billType">{billTypeToName[item.useFor]}</div>
+                            </div>
+                            <div className={classNames('money', item.type)}>
+                                {item.money.toFixed(2)}
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
